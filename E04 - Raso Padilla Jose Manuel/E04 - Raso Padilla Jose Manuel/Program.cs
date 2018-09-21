2cfe70281b70e10f26dd81fbb72ae140d6feec57
Jose Manuel Raso Padilla <josmanpadilla@hotmail.com>
2018-09-20 19:17:41 -0700

    Ejercicios

---
+++ E04 - Raso Padilla Jose Manuel/E04 - Raso Padilla Jose Manuel/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E04___Raso_Padilla_Jose_Manuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numeros = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                int suma = 0;
+                int contador;
+                SumaNumeros4();
+                SumaNumeros9();
+                Console.ReadLine();
+
+                void SumaNumeros4()
+                {
+                    for (contador = 0; contador < 4; contador++)
+                    {
+                        suma = numeros[contador] + suma;
+                    }
+                    Console.Write("La suma de los primeros 4 elementos es de: {0}\n\n", suma);
+                    suma = 0;
+                }
+                void SumaNumeros9()
+                {
+                    for (contador = 0; contador < 9; contador++)
+                    {
+                        suma = numeros[contador] + suma;
+                    }
+                    Console.Write("La suma de los elementos 1 a 9 es de: {0}\n\n", suma);
+                    suma = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Error encontrado: {0}", ex);
+            }
+        }
+    }
+}
---
+++ Temperatura/Temperatura/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Temperatura
+{
+    public class Temperatura
+    {
+        public double TemperaturaGrados;
+
+        public double CalcularCelcius(double Valor)
+        {
+           TemperaturaGrados = Math.Round((Valor * 1.8) + 32, 2);
+            return TemperaturaGrados;
+        }
+
+        public double CalcularFahrenheit(double Valor)
+        {
+            TemperaturaGrados = Math.Round((Valor - 32) / 1.8, 2);
+            return TemperaturaGrados;
+        }
+    }
+}
