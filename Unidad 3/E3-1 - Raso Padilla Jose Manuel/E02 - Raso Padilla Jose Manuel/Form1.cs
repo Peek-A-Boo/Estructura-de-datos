8a669a420acdb821a76e9d16281821e6c6c33cee
Jose Manuel Raso Padilla <josmanpadilla@hotmail.com>
2018-10-10 00:12:17 -0700

    Mejorando la clase

---
+++ Unidad 3/E3-1 - Raso Padilla Jose Manuel/E02 - Raso Padilla Jose Manuel/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+
+namespace E02___Raso_Padilla_Jose_Manuel
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList NombresAlumnos = new ArrayList(); //ArrayList para el nombre de los alumnos
+        ArrayList Calificaciones = new ArrayList(); //ArrayList para las calificaciones
+        ArrayList Largos = new ArrayList(); //ArrayList creado para almacenar los largos de donde empieza cada clase y donde termina
+        int acumulador = 0; //acumulador donde se guarda los largos(utilizado en Largos)
+        Funciones Objeto = new Funciones(); //Objeto de la clase donde se realizan las operaciones
+
+
+        private void btnCargar_Click(object sender, EventArgs e) //Evento al dar click donde se llama al metodo de CargarClase con los valores de referencia
+        {                                                        //siendo los ArrayList y el acumulador ademas se manda las ListBox de alumnos y clases como valor
+            Objeto.CargarClase(ref NombresAlumnos,ref Calificaciones,ref Largos,lstAlumnos,lstClases,ref acumulador);
+        }
+
+        private void btnDesplegar_Click(object sender, EventArgs e) //Evento que al dar click se llama al metodo de desplegar pidiendo como parametros
+        {                                                           //los ArrayList de nombres alumnos, calificaciones, el largo de donde empieza dicha clase        
+            try                                                     //y donde acaba siendo los contador 1 y 2 respectivamente y la ListBox alumnos para desplegar los resultados.
+            {
+                lblNombreClase.Text = lstClases.Text; //Se pone el nombre de la clase como "titulo".
+                lstAlumnos.Items.Clear(); //Se limpia la lista para que no se muestren los datos del despliegue anterior
+                Objeto.DesplegarClase(NombresAlumnos, Calificaciones, Convert.ToInt32(Largos[lstClases.SelectedIndex * 2]), Convert.ToInt32(Largos[((lstClases.SelectedIndex * 2) + 1)]),lstAlumnos);
+            }  //la formula para el contador fue un poco de logica debido a que el indice 0 y 1 del largo son para la clase 1, el 2 y 3 para la clase 2 . . . con la formula de SelectedIndex * 2 y (SelectedIndex * 2)+1
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error encontrado: " + ex.ToString());
+            }
+        }
+    }
+}
---
+++ Unidad 3/E3-1 - Raso Padilla Jose Manuel/E02 - Raso Padilla Jose Manuel/Funciones.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace E02___Raso_Padilla_Jose_Manuel
+{
+    class Funciones
+    {
+        public void DesplegarClase(ArrayList NombresAlumnos, ArrayList Calificaciones,int contador, int contador2, ListBox lstAlumnos) //Se reciben los parametros
+        {
+            for(contador=contador;contador<contador2;contador++) //Realiza el despliegue concatenando el nombre y la calificacion en base al indice que tienen
+            {  //Los indices (contador y contador2) estan en base a los largos previamente mencionados donde se ponia donde empezaba y terminaba cada clase.
+                string cadena = Convert.ToString(NombresAlumnos[contador]) + " - " + Convert.ToString(Calificaciones[contador]);
+                lstAlumnos.Items.Add(cadena);
+            }
+        }
+
+        public void CargarClase(ref ArrayList NombresAlumnos, ref ArrayList Calificaciones, ref ArrayList Largos, ListBox lstAlumnos,ListBox lstClases,ref int acumulador)
+        {   //Metodo para cargar clases en las listas
+            try
+            {
+                Largos.Add(acumulador);//se guarda el indice donde empieza la clase.
+                string nombreClase = Interaction.InputBox("Ingrese nombre de la clase", "Nombre",""); //Se ingresan el nombre y calificacion de la clase mediante un inputbox
+                int cantidadAlumnos = Convert.ToInt32(Interaction.InputBox("Ingrese cantidad de alumnos", "Cantidad","0"));
+                Largos.Add(cantidadAlumnos + acumulador); //Se pone el indice de donde acaba la clase dentro de la ArrayList de largos
+                lstClases.Items.Add(nombreClase); //Se agrega el nombre de la clase a la ListBox para las clases
+                lstClases.SelectedIndex = lstClases.SelectedIndex+1; //Se selecciona la clase creada
+                for(int contadorSubida=0;contadorSubida<cantidadAlumnos;contadorSubida++) //Ingresa los alumnos
+                {
+
+                    string nombre = Interaction.InputBox("Ingrese nombre del alumno " + (contadorSubida + 1), "Nombre del alumno " + (contadorSubida + 1),"Desconocido");
+                    int calificacion = Convert.ToInt32(Interaction.InputBox("Ingrese calificacion del alumno " + (contadorSubida + 1), "Calificacion del alumno " + (contadorSubida + 1),"0"));
+                    NombresAlumnos.Add(nombre);
+                    Calificaciones.Add(calificacion);
+                }
+                acumulador = acumulador + cantidadAlumnos; //El acumulador se suma con la cantidad de alumnos y como es valor de referencia es modificado en el form 1
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error encontrado\n\n");
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
